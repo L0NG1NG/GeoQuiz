094646169f37919f51a3cdc0737e9e5dba912cbd
L0NG1NG <610306739@qq.com>
2021-05-25 20:34:13 +0800

    从按钮到图标按钮

--- app/src/main/java/com/longing/geoquiz/MainActivity.kt
+++ app/src/main/java/com/longing/geoquiz/MainActivity.kt
@@ -2,14 +2,18 @@ package com.longing.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -30,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener {
             checkAnswer(true)
@@ -42,6 +47,16 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener {
+            currentIndex = if (currentIndex == 0) currentIndex
+            else currentIndex - 1
+            updateQuestion()
+        }
+
+        questionTextView.setOnClickListener {
             updateQuestion()
         }
 
@@ -64,7 +79,9 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
 
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .apply { setGravity(Gravity.TOP, 0, 0) }
+            .show()
     }
 
 
